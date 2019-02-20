6424b15482ea241777c983c5002f5f6e90c82a5f
Fabio Rosano <fabio.rosano.anc@gmail.com>
2019-02-20 11:54:28 +0900

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,8 +7,8 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Malware Payloads : Microsoft Office macros se nafatin dala barak liu husi uza metodu prestasaun</h1>
+    <p>Iha atividade nee fó hanoin katak kuandu malware halo prestasaun, fo ameasa ba autor sira iha tendénsia atu hakarak kokoiha dalan nebe bele asesu tama. Roundup ida neebé foin lalais nee husi Intelijénsia Cofense inklui nian loaders husi malware analiza tiha ona ho volume ba fulan-Agostu 2018. Nia hetan katak dokumentu Microsoft Office hatama macros basá konta 45% hetan hotu-hotu. Metodu segundu populár liu (kontabilidade ba kazu 37%), nebé involve esplorasaun ba CVE-2017-11882, ida Microsoft Eskritóriu Memória Korrupsaun Vulner</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
