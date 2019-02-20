593ab44b0ae0cbd17db1db7c0f3a9f6bad038587
Fabio Rosano <fabio.rosano.anc@gmail.com>
2019-02-20 11:40:06 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <h1>Bemvidu mai portal Siber Seguransa</h1>
     <p>Seguransa Cyber no privasidade dadus sempre tópiku ida nebe manas bainhira ita koalia kona-ba. Sira iha fatin barak iha fatin neʼebé ema bele hetan liafuan interesante, análize, komentáriu no hetan informasaun kona-ba ameasa ikus no oinsá atu lida ho sira ho efisiénsia. Husi developers ba seguransa peskizador, empreza software website seguransa no magazines ema hotu-hotu iha opiniaun ida kona-ba. Ida nee la fasil atu halo diferensa entre sira no kuandu imi buka seguransa online blog/liafuan imi sei sai ema rihun ba rihun.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
